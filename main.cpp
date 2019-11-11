d0e2418137a895bd581c762b3a62df326aba8ddd
ioChris369 <alexnp62@gmail.com>
2019-11-11 05:09:33 -0800

    planning argparser

--- main.cpp
+++ main.cpp
@@ -5,7 +5,31 @@
 #include <ctype.h>
 
 
+unsigned int intMagnitudeFromChar(std::string numChar){
+	//we reveive a pointer as an argument
 
+	int magnitude{0}; //we'll adding each interation to this variable
+	
+	int digitPower{static_cast<int>(numChar.size()) - 1};
+	
+	for(char tmpChar: numChar){
+		switch(tmpChar){
+			case '9': magnitude += 9 * std::pow(10, digitPower); digitPower--; break;
+			case '8': magnitude += 8 * std::pow(10, digitPower); digitPower--; break;
+			case '7': magnitude += 7 * std::pow(10, digitPower); digitPower--; break;
+			case '6': magnitude += 6 * std::pow(10, digitPower); digitPower--; break;
+			case '5': magnitude += 5 * std::pow(10, digitPower); digitPower--; break;
+			case '4': magnitude += 4 * std::pow(10, digitPower); digitPower--; break;
+			case '3': magnitude += 3 * std::pow(10, digitPower); digitPower--; break;
+			case '2': magnitude += 2 * std::pow(10, digitPower); digitPower--; break;
+			case '1': magnitude += 1 * std::pow(10, digitPower); digitPower--; break;
+			case '0': magnitude += 0 * std::pow(10, digitPower); digitPower--; break;
+			default: std::cerr << "\n \t something unexpected just happened \n" << std::endl; break;
+		}
+	}
+
+	return magnitude;
+}	
 
 bool isValidNumber(std::string strNum){ 
 	
